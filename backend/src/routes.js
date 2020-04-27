2e799625ad7834c54ffa5dc738bcf76eb965c576
Odenir <odenirdev@gmail.com>
2020-04-27 13:33:38 -0300

    Setting upload url

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,16 +7,26 @@ const mongoose = require('mongoose');
 const Post = require("./models/Post");
 
 routes.post('/posts', multer(multerConfig).single('file'), async(req, res) => {
-    const { originalname: name, size, filename: key } = req.file;
+    try {
+        const { originalname: name, size, filename: key, path: url } = req.file;
 
-    const post = await Post.create({
-        name,
-        size,
-        key,
-        url: ''
-    });
+        const post = await Post.create({
+            name,
+            size,
+            key,
+            url
+        });
 
-    return res.json(post);
+        return res.json(post);
+    } catch (error) {
+        res.status(400).send({
+            error: {
+                pt: 'Upload de arquivo falhou',
+                en: 'Upload file fail'
+            }
+        });
+    }
+
 });
 
 module.exports = routes;
